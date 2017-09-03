48af3d89a2bc00c3ec0d740dac90594190a7f03a
Pawel Maricnkowski <wafel34@gmail.com>
2017-09-03 13:08:28 +0100

    Added conditional formatting for dates. Explained in calendar.js comment

--- app/routes/calendar.js
+++ app/routes/calendar.js
@@ -3,11 +3,33 @@ var express = require('express'),
 
 
 router.get('/calendar', function(req, res){
-    var calendar = req.app.get('calendar');
+
+    // Below i'm getting current date and pare it as well as parse dates of competitions.
+    // Parsed dates are then compared in 'view_calendar.ejs' and if current date is bigger than date of certain competition
+    // CSS style will be applied to indicate that event already happened.
+
+    var calendar = req.app.get('calendar'),
+        today = new Date(),
+        today = Date.parse(today),
+        dates = calendar.map(function(item){
+            var temp = item.date.split('/'),
+                date = new Date(temp[2], temp[1], temp[0]);
+                date = Date.parse(date);
+            var result = {
+                "place": item.place,
+                "country": item.country,
+                "date": item.date,
+                "type": item.type,
+                dateObj: date
+            };
+            return result;
+        });
+
 
     res.render('view_calendar', {
-        calendar: calendar,
-        pageId: 'Calendar'
+        calendar: dates,
+        pageId: 'Calendar',
+        today: today
     });
 });
 
