77d6d93c9d7ae0ea12a1b71d936de155c0349a18
Pawel Marcinkowski <pmarcinkowski1016@timeinc.com>
2017-09-04 13:29:25 +0100

    Deleted resutl

--- app/routes/calendar.js
+++ app/routes/calendar.js
@@ -35,20 +35,15 @@ router.get('/calendar', function(req, res){
 
 router.get('/:hill', function(req, res){
 
-    // Below i'm getting current date and pare it as well as parse dates of competitions.
-    // Parsed dates are then compared in 'view_calendar.ejs' and if current date is bigger than date of certain competition
-    // CSS style will be applied to indicate that event already happened.
 
     var calendar = req.app.get('calendar'),
         hill = req.params.hill,
         len = calendar.length,
         data = {},
-        result = "",
         i = 0;
 
         for (i=0; i<len; i+=1){
             if(calendar[i].place === hill) {
-                result = hill;
                 data = calendar[i];
                 break;
             }
